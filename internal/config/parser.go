86f7572b16def2b00257b514693113f3e880aa31
Hugh Fitzpatrick <hugh.fitzpatrick2@ucdconnect.ie>
2025-07-25 14:44:01 +0100

    update: error printing for clarity
    Error printing now details the go file throwing an error. TODO: Add
    proper error logging.

--- internal/config/parser.go
+++ internal/config/parser.go
@@ -21,13 +21,13 @@ func ReadYamlConfig(filename string) (*YamlConfig, error) {
 	var config YamlConfig
 
 	if err := yaml.Unmarshal(f, &config); err != nil {
-		return nil, fmt.Errorf("error unmarshalling config file: %w", err)
+		return nil, fmt.Errorf("config/parser: error unmarshalling config file: %w", err)
 	}
 
 	validator := validator.New()
 
 	if err := validator.Struct(&config); err != nil {
-		return nil, fmt.Errorf("validation failed. %w", err)
+		return nil, fmt.Errorf("config/parser: validation failed. %w", err)
 	}
 	return &config, nil
 }
@@ -42,14 +42,14 @@ func ConvertConfig(yamlConfig *YamlConfig) (*HealthCheckConfig, error) {
 	for _, endpoint := range yamlConfig.Endpoints {
 		joinedURL, err := url.JoinPath(baseURL, endpoint)
 		if err != nil {
-			return nil, fmt.Errorf("error joining URL path: %w", err)
+			return nil, fmt.Errorf("config/parser: error joining URL path: %w", err)
 		}
 		URLs = append(URLs, joinedURL)
 	}
 
 	cfg.URLs = URLs
 	if err := cfg.Validate(); err != nil {
-		return nil, fmt.Errorf("error in URLs created from config: %w", err)
+		return nil, fmt.Errorf("config/parser: error in URLs created from config: %w", err)
 	}
 	cfg.Timeout = time.Millisecond * time.Duration(yamlConfig.Timeout)
 	return &cfg, nil
@@ -59,7 +59,7 @@ func (hc *HealthCheckConfig) Validate() error {
 	for _, urlStr := range hc.URLs {
 		parsedURL, err := url.Parse(urlStr)
 		if err != nil || parsedURL.Scheme == "" || parsedURL.Host == "" {
-			return fmt.Errorf("invalid URL generated: %s (error: %w)", urlStr, err)
+			return fmt.Errorf("config/parser: invalid URL generated: %s (error: %w)", urlStr, err)
 		}
 	}
 	return nil
